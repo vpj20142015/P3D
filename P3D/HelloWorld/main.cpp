eddaf8c39f6e1630aae2daad51f3350acf556f74
pedroabgmarques <pedroabgmarques@gmail.com>
2016-02-26 10:52:02 +0000

    Alguns printfs

--- P3D/HelloWorld/main.cpp
+++ P3D/HelloWorld/main.cpp
@@ -7,18 +7,23 @@ void renderScene(void)
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(1.0, 0.0, 0.0, 1.0); //clear red
+	
+	printf("Scene rendered.\n");
 
 	glutSwapBuffers();
 }
 
 int main(int argc, char **argv)
 {
+	printf("Program started.\n");
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 20);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("OpenGL First Window");
+
+	printf("Window created.\n");
 
 	glEnable(GL_DEPTH_TEST);
 
